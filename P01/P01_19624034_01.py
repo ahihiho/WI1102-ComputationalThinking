7dd3af5a3e2e5938c277f3ba7a68f65845e9315e
Ghaisan N <123650700+ghaisannfl@users.noreply.github.com>
2025-04-10 00:30:13 +0700

    P01_19624034_01.py

---
+++ P01/P01_19624034_01.py
@@ -0,0 +1,21 @@
+"""
+NIM/NAMA   : 19624034/GN
+TANGGAL    : 17/10/2024
+DESKRIPSI  : program menentukan nilai tukar terbesar
+"""
+
+#KAMUS
+# peng, kom : float
+
+#ALGORITMA
+##input
+peng  = float(input('Banyak uang Peng yang ditawarkan: '))
+kom   = float(input('Banyak uang Kom yang ditawarkan: '))
+peng *= float(input('Konversi mata uang Peng ke rupiah: ')) 
+kom  *= float(input('Konversi mata uang Kom ke rupiah: '))
+
+##proses + output
+if peng > kom:
+  print('Adik Tuan Leo memilih uang Peng.')
+else: # uang Kom > uang Peng
+  print('Adik Tuan Leo memilih uang Kom.')
