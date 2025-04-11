c719bb100474d5d0bd46894706fcaf67416fd674
Ghai <123650700+ghaisannfl@users.noreply.github.com>
2025-04-11 16:05:00 +0700

    Rename file

--- P01/P01_19624034_02.py
+++ P01/P01_19624034_02.py
@@ -25,5 +25,5 @@ if ((a1 != a2 and a1 != a3 and a1 != a4 and a2 != a3 and a2 != a4 and a3 != a4) 
   else: # bilangan unik
     print("Bilangan tersebut adalah bilangan Unik.")
 else: # tidak memenuhi kriteria (bilangan biasa)
-print("Bilangan tersebut adalah bilangan Biasa.")
+  print("Bilangan tersebut adalah bilangan Biasa.")
       
