0f70f913252a3d2dd4f4d28ea8b5b172c032fa11
Ghai <123650700+ghaisannfl@users.noreply.github.com>
2025-04-13 19:43:49 +0700

    Update file

---
+++ SolusiModul/M1_2024_02.py
@@ -0,0 +1,18 @@
+# DESKRIPSI : Program memastikan apakah Tuan Riz berhasil menyelesaikan soal atau tidak
+
+#KAMUS
+# isian, esai, sisaIsian, sisaEsai, total : integer
+
+#ALGORITMA
+##input
+isian  = int(input("Masukkan banyak soal isian singkat yang sudah diselesaikan: ")) 
+esai   = int(input("Masukkan banyak soal esai yang sudah diselesaikan: "))
+sisaIsian  = (14 - isian) * 10
+sisaEsai   = (2 - esai) * 20
+total  = sisaIsian + sisaEsai 
+
+##proses + output
+if (total < 60):
+  print("Tuan Riz akan berhasil mengerjakan semua soal")
+else:
+  print("Tuan Riz tidak berhasil mengerjakan semua soal tepat waktu")
