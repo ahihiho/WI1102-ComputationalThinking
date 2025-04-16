cf81e92940bbf18a85ef679e395bed9b40d2fd02
Ghai <123650700+ghaisannfl@users.noreply.github.com>
2025-04-16 21:33:38 +0700

    Update file

--- P02/P02_19624034_01.py
+++ P02/P02_19624034_01.py
@@ -1,5 +1,25 @@
 """
-NIM/NAMA   : 19624034/GN
-TANGGAL    : 31/10/2024
-DESKRIPSI  :
+NIM/Nama   : 19624034/Ghaisan Nufail
+Tanggal    : 31/10/2024
+Deskripsi  : Mencari banyak angka yang dapat dibagi oleh satuannya
 """
+
+#KAMUS
+# a, b, count  : integer
+
+#ALGORITMA
+##input
+a = int(input("Masukkan nilai A: "))
+b = int(input("Masukkan nilai B: "))
+
+##proses
+count = 0 # inisialisasi counter 
+for i in range (a, b+1): # loop dari nilai a hingga nilai b
+    if i % 10 != 0:      # syarat : digit terakhir bukan nol
+        if i %(i%10)==0: # sifat  : bilangan habis dibagi digit akhir
+         count +=1 
+
+##output
+print(f"Terdapat {count} angka yang memenuhi sifat tersebut. ")
+
+#Selesai :D
