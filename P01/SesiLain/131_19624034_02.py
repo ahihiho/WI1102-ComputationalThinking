8c2b4651ff4d65db3a41939efa8ac7ed28f98cfd
Ghai <123650700+ghaisannfl@users.noreply.github.com>
2025-04-18 23:22:52 +0700

    Update ASAP

--- P01/SesiLain/131_19624034_02.py
+++ P01/SesiLain/131_19624034_02.py
@@ -1,9 +1,21 @@
+"""
+NIM/NAMA  : 19624034/GN
+TANGGAL   : 14/10/2024
+DESKRIPSI : 
+"""
+
+#KAMUS
+# n  : integer
+# tv : string
+
+#ALGORITMA
+##input
 n = int(input("Jumlah perangkat Tuan Mik dan teman-temannya:"))
 tv = input("Apakah ingin akses ke TV Channel?(Ya atau Tidak): ")
 
 if (n <= 3) and (tv == "Tidak"):
   print("Tuan Mik akan memesan 1 Paket A dengan total harga 250 ribu")
-elif (n > 3 and n < 7 and tv == "Ya") or (tv =="Ya"):
+elif (n > 3 and n < 7) or (tv =="Ya"):
   print("Tuan Mik akan memesan 1 Paket B dengan total harga 375 ribu")
 elif ( n >= 7 and n <= 12):
   print("Tuan Mik akan memesan 1 Paket C dengan total harga 600 ribu")
