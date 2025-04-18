e6d391f090ab5321ac27f0cbfa98d33960a92599
Ghai <123650700+ghaisannfl@users.noreply.github.com>
2025-04-18 23:19:12 +0700

    Update ASAP

--- P01/SesiLain/131_19624034_02.py
+++ P01/SesiLain/131_19624034_02.py
@@ -1,9 +1,14 @@
 n = int(input("Jumlah perangkat Tuan Mik dan teman-temannya:"))
 tv = input("Apakah ingin akses ke TV Channel?(Ya atau Tidak): ")
 
-if (n <= 3) and (tv = "Tidak"):
+if (n <= 3) and (tv == "Tidak"):
   print("Tuan Mik akan memesan 1 Paket A dengan total harga 250 ribu")
 elif (n > 3 and n < 7 and tv == "Ya") or (tv =="Ya"):
   print("Tuan Mik akan memesan 1 Paket B dengan total harga 375 ribu")
-elif ( n >= 8 and n <= 12):
+elif ( n >= 7 and n <= 12):
   print("Tuan Mik akan memesan 1 Paket C dengan total harga 600 ribu")
+elif (n > 12):
+  n = n/12
+  if (n > 1 and n % 12 == 0):
+    print(f"Tuan Mik akan memesan {n} Paket C dengan total harga", (600*n), "ribu")
+  
