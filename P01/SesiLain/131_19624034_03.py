3352aab686a768fc06a50b069a2b8bcef5c9c45f
Ghai <123650700+ghaisannfl@users.noreply.github.com>
2025-04-20 09:07:42 +0700

    Soon

---
+++ P01/SesiLain/131_19624034_03.py
@@ -0,0 +1,19 @@
+"""
+NIM/NAMA  : 19624034/GN
+TANGGAL   : 14/10/2024
+DESKRIPSI : 
+"""
+
+#KAMUS
+# o, h, u, ukurO, ukurH, ukurU : integer
+
+#ALGORITMA
+##input
+o = int(input("Banyak barang oranye: ")) # 
+h = int(input("Banyak barang hijau: "))  # 
+u = int(input("Banyak barang ungu: "))   # 
+ukurO = int(input("Ukuran barang oranye (unit): ")) # 
+ukurH = int(input("Ukuran barang hijau (unit): "))  # 
+ukurU = int(input("Ukuran barang ungu (unit): "))   # 
+
+##proses
