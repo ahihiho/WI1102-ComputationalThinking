c5a699b2ef7f9c0d1f1643b81b4c49f5238725a8
Ghai <123650700+ghaisannfl@users.noreply.github.com>
2025-04-27 22:36:31 +0700

    Update file

--- P01/SesiLain/131_19624034_03.py
+++ P01/SesiLain/131_19624034_03.py
@@ -15,6 +15,9 @@ u = int(input("Banyak barang ungu: "))   #
 ukurO = int(input("Ukuran barang oranye (unit): ")) # 
 ukurH = int(input("Ukuran barang hijau (unit): "))  # 
 ukurU = int(input("Ukuran barang ungu (unit): "))   # 
+m = 1
+k = 1
+b = 1
 
 ##proses
 totalO = o * ukurO
@@ -22,5 +25,19 @@ totalH = h * ukurH
 totalU = u * ukurU
 
 if (totalO > 30):
-  o = totalO % 2 + totalO // 2
+  o = o % 2 + o // 2
   totalO = o * ukurO
+else:
+  if (total > 20):
+    print("Barang oranye disimpan di Container Kuning")
+    k -= 1
+    if(totalH < 35):
+      print("Barang oranye disimpan di Container Biru")
+    else:
+      h = h % 2 + h // 2
+      totalH = h * ukurH
+      if (totalH < 35):
+      print("Barang oranye disimpan di Container Biru")
+  else:
+    print("Barang oranye disimpan di Container Merah")
+    m -= 1
