860be453268097f817757e3c9868397227b21ac5
Ghai <123650700+ghaisannfl@users.noreply.github.com>
2025-04-28 18:48:14 +0700

    Update soon

--- P01/SesiLain/131_19624034_03.py
+++ P01/SesiLain/131_19624034_03.py
@@ -15,9 +15,6 @@ u = int(input("Banyak barang ungu: "))   #
 ukurO = int(input("Ukuran barang oranye (unit): ")) # 
 ukurH = int(input("Ukuran barang hijau (unit): "))  # 
 ukurU = int(input("Ukuran barang ungu (unit): "))   # 
-m = 1
-k = 1
-b = 1
 
 ##proses
 totalO = o * ukurO
@@ -27,17 +24,29 @@ totalU = u * ukurU
 if (totalO > 30):
   o = o % 2 + o // 2
   totalO = o * ukurO
-else:
-  if (total > 20):
-    print("Barang oranye disimpan di Container Kuning")
-    k -= 1
-    if(totalH < 35):
-      print("Barang oranye disimpan di Container Biru")
+
+if (totalO > 30): 
+  print("Barang oranye tidak cukup untuk disimpan")
+  
+elif (totalO > 20):
+  print("Barang oranye disimpan di Container Kuning")
+  if(totalH < 35):
+    print("Barang oranye disimpan di Container Biru")
+    if(totalU <= 20):
+      print("Barang oranye disimpan di Container Merah")
     else:
-      h = h % 2 + h // 2
-      totalH = h * ukurH
-      if (totalH < 35):
-      print("Barang oranye disimpan di Container Biru")
+      u = u % 2 + u // 2
+      totoalU = u * ukurU
+      if totalU <= 20: 
+        print("Barang oranye disimpan di Container Merah")
+      else: 
+        print("Barang hijau tidak cukup untuk disimpan")
   else:
-    print("Barang oranye disimpan di Container Merah")
-    m -= 1
+     h = h % 2 + h // 2
+     totalH = h * ukurH
+    if (totalH < 35):
+     print("Barang oranye disimpan di Container Biru")
+else:
+print("Barang oranye disimpan di Container Merah")
+ 
+# Nanti di update yaw :D
