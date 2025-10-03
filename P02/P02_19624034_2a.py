19aae328bb2038449a48486948833f0c8642514a
Ghai <123650700+ghaisannfl@users.noreply.github.com>
2025-10-03 22:32:43 +0700

    Add files via upload

---
+++ P02/P02_19624034_2a.py
@@ -0,0 +1,40 @@
+"""
+NIM/Nama   : 19624034/GN
+Tanggal    : 31/10/2024
+Deskripsi  : Mengubah pesan biasa menjadi rahasia
+"""
+
+#KAMUS
+# n  : integer
+# biasa, rahasia : list of character
+# pesan, pesanRahasia : string
+
+#ALGORITMA
+##input
+n       = int (input("Masukkan jumlah huruf pada kamus rahasia: ")) # input banyaknya huruf yang ingin diubah
+biasa   = [""] * n # list yang akan menyimpan huruf biasa (berukuran n)
+rahasia = [""] * n # list yang akan menyimpan huruf rahasia (berukuran n)
+
+##proses 
+for i in range(n):
+    biasa[i] = input(f"Masukkan huruf biasa ke-{i+1}: ")
+    rahasia[i] = input(f"Masukkan huruf rahasia ke-{i+1}: ")
+    
+
+    
+pesan = input("Masukkan pesan yang ingin diubah: ") # input pesan
+
+## proses enkripsi pesan
+pesanRahasia = "" # variable string kosong untuk menampung hasil pesan rahasia
+for huruf in pesan: # loop yang membaca tiap character dalam string pesan
+    for i in range(n):
+        if huruf == biasa[i]: # jika char sama dengan huruf biasa
+            pesanRahasia += rahasia[i] # huruf rahasia akan ditambah ke string pesanRahasia
+            break # exit loop
+    else: # huruf not equal dengan list huruf biasa
+        pesanRahasia += huruf
+
+##output
+print(f"Pesan rahasia Nona Sal: \n{pesanRahasia}")
+
+#Selesai :D
