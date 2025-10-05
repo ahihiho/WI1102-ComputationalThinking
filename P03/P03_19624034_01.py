0f8c482b85d2e1ae6343ac89f5cef91372a295d1
Ghai <123650700+ghaisannfl@users.noreply.github.com>
2025-10-05 08:56:11 +0700

    refactor: update readability

--- P03/P03_19624034_01.py
+++ P03/P03_19624034_01.py
@@ -7,33 +7,35 @@ DESKRIPSI  : Program yang menentukan jenis persamaan kuadrat berdasarkan nilai d
 #KAMUS
 # diskriminan : fungsi
 # persamaan_kuadrat : prosedur
-# a, b, c, d : integer
+# a, b, c : integer
+# d : float
 
 #ALGORITMA
 ##fungsi diskriminan
 def diskriminan(a, b, c):
-    return float (b * b) - (4 * a * c)
+    return float (b * b) - (4 * a * c) # mengembalikan nilai diskriminan
      
 ##prosedur menentukan persamaan kuadrat
 def persamaan_kuadrat(nilai_diskriminan):
     print("Persamaan kuadrat ", end="")
-    if (nilai_diskriminan > 0):
+    if (nilai_diskriminan > 0): # jika diskriminan lebih dari nol
         print("memiliki akar berbeda")
-    elif (nilai_diskriminan < 0):
+    elif (nilai_diskriminan < 0): # jika diskriminan kurang dari nol
         print("tidak memiliki akar real")
-    else:
+    else: # jika diskriminan = nol
         print("memiliki akar kembar")
         
 ##input
+# masukkan nilai koefisien fungsi
 a = int(input("Masukkan nilai a: "))
 b = int(input("Masukkan nilai b: "))
 c = int(input("Masukkan nilai c: "))
 
 ##proses 
-d = diskriminan(a, b, c)
+d = diskriminan(a, b, c) # memanggil fungsi diskriminan dan menyimpan nilai di d
 
 ##output
-print(f"Nilai diskriminan: {d:.2f}")
-persamaan_kuadrat(d) 
+print(f"Nilai diskriminan: {d:.2f}") 
+persamaan_kuadrat(d)  # memanggil prosedur menentukan jenis persamaan kuadrat
 
 #Selesai :D
